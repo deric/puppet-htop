01c05892038e168c8754a53d44bb622d31af7605
Tomas Barton <barton.tomas@gmail.com>
2023-09-14 13:37:15 +0200

    Don't fail on unsupported systems

--- spec/classes/htop_spec.rb
+++ spec/classes/htop_spec.rb
@@ -113,17 +113,4 @@ describe 'htop' do
       end
     end
   end
-
-  context 'unsupported operating system' do
-    describe 'htop class without any parameters on Solaris/Nexenta' do
-      let(:facts) do
-        {
-          osfamily: 'Solaris',
-          operatingsystem: 'Nexenta',
-        }
-      end
-
-      it { expect { is_expected.to contain_package('htop') }.to raise_error(Puppet::Error, %r{Nexenta not supported}) }
-    end
-  end
 end
